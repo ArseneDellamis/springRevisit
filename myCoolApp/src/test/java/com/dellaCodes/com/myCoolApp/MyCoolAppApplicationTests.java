5072e662ab4acf62ad6c17a153474de0fe5a35b5
ArseneDellamis <dellamis11@gmail.com>
2026-06-29 09:24:20 +0200

    first commit

---
+++ myCoolApp/src/test/java/com/dellaCodes/com/myCoolApp/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dellaCodes.com.myCoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
