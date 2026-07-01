6026a6f121de229158a12bc619eff0ecdeb94a7f
ArseneDellamis <dellamis11@gmail.com>
2026-07-01 10:54:36 +0200

    adding configurations in properties and add an endpoint

--- myCoolApp/src/main/java/com/dellaCodes/com/myCoolApp/controller/FunRestController.java
+++ myCoolApp/src/main/java/com/dellaCodes/com/myCoolApp/controller/FunRestController.java
@@ -1,10 +1,24 @@
 package com.dellaCodes.com.myCoolApp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+//    injecting properties
+
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+//    endpoint to sue the coach, and team info
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "coach " + coachName + " ,team name" + teamName;
+    }
 
 //exposed "/" that turn "hello world"
     @GetMapping("/")
