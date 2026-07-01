42ec35942a1044bab9734b54a4d249b16c4c7c71
ArseneDellamis <dellamis11@gmail.com>
2026-07-01 12:02:17 +0200

    adding project structure

---
+++ src/test/java/com/devcode/devspringboot/DevSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcode.devspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
